49b816014ad3c8ac55fe0d11f7a3db332c7b8cbd
lucasvlrn <lucasvilarinhop@gmail.com>
2025-04-23 20:21:31 -0300

    melhorando indentação e adicionando variaveis de ambiente para não vazar credenciais

--- index.php
+++ index.php
@@ -1,48 +1,40 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-<title>Exemplo PHP</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
+
 <body>
+  <?php
+  ini_set("display_errors", 1);
+  header('Content-Type: text/html; charset=iso-8859-1');
 
-<?php
-ini_set("display_errors", 1);
-header('Content-Type: text/html; charset=iso-8859-1');
+  echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
+  //Conexão
+  $servername = $_ENV["DB_HOST"];
+  $username = $_ENV["DB_USERNAME"];
+  $password = $_ENV["DB_PASSWORD"];
+  $database = $_ENV["DB_DATABASE"];
 
+  $dns = "mysql:host=$servername; dbname=$database";
+  $pdo = new PDO($dns, $username, $password);
 
-echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
+  $valor_rand1 = rand(1, 999);
+  $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
+  $host_name = gethostname();
 
-$servername = "54.234.153.24";
-$username = "root";
-$password = "Senha123";
-$database = "meubanco";
+  $query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
 
-// Criar conexão
-
-
-$link = new mysqli($servername, $username, $password, $database);
-
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-
-$valor_rand1 =  rand(1, 999);
-$valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
-$host_name = gethostname();
-
-
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
-
-
-if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $link->error;
-}
-
-?>
+  if ($link->query($query) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $link->error;
+  }
+  ?>
 </body>
+
 </html>
